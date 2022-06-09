983f50a72a1eac0b8d629b75446c487be516100e
Erik Nordqvist <nordqvist.eriken@gmail.com>
2022-06-09 20:30:26 +0200

    finished layout before starting on exec for local scripts

--- django_iot/my_iot_site/home/views.py
+++ django_iot/my_iot_site/home/views.py
@@ -1,22 +1,25 @@
+import time
+import os
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+
 def home_view(request):
     return render(request, "home.html", {})
 
 
-def iot_view(request):
+def iot_view(request, *args, **kwargs):
+    print(args)
     return render(request, "iot.html", {})
 
 
 def spotify_view(request):
-    return HttpResponse("this is Spotify")
+    return render(request, "spotify.html", {})
 
 
 def stats_view(request):
-    return HttpResponse("this is stats")
+    return render(request, "stats.html", {})
 
 
 def weather_view(request):
-    return HttpResponse("this is the weather")
+    return render(request, "weather.html", {})
