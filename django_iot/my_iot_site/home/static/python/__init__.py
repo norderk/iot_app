9d20069c665ebe92c75371a20609866a789e9628
Erik Nordqvist <nordqvist.eriken@gmail.com>
2022-07-23 18:21:05 +0200

    some other shit..

---
+++ django_iot/my_iot_site/home/static/python/__init__.py
@@ -0,0 +1,4 @@
+import  os
+os.environ(MQTT_BROKER_HOST="192.168.0.17")
+os.environ(MQTT_BROKER_HOST=1883)
+os.environ(MQTT_KEEP_ALIVE_INTERVAL=60)
---
+++ django_iot/my_iot_site/home/static/python/lights.py
@@ -0,0 +1,49 @@
+import paho.mqtt.client as mqtt
+import time
+import os
+
+MQTT_BROKER_HOST = str(os.getenv('MQTT_BROKER_HOST'))
+MQTT_BROKER_PORT = int(os.getenv('MQTT_BROKER_PORT'))
+MQTT_KEEP_ALIVE_INTERVAL = int(os.getenv('MQTT_KEEP_ALIVE_INTERVAL'))
+
+def on_connect(client, userdata, flags, rc):
+    
+    if rc == 0:
+        print("Connected - OK\n")
+        client.subscribe("zigbee2mqtt/living_room")
+    else:
+        print("Failed to connect to MQTT broker, return code ", rc)
+        print("\n")       
+
+def on_message(client, userdata, msg):    
+    print("calling on_message:\n")
+    print(msg.topic + " " + str(msg.payload))
+
+def on_log(client, userdata, level, buf):
+    print("log: ", buf)
+
+def on_disconnect(client, userdata, flags, rc=0):
+    print("Disconnected result code "+str(rc))
+
+
+def call_mqtt(topic, payload):
+    print(topic)    
+    print(payload)
+    topic = topic
+    payload = payload
+
+    client = mqtt.Client("myClient")
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    client.on_log = on_log
+    client.on_message = on_message
+
+    client.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT, MQTT_KEEP_ALIVE_INTERVAL)
+    # client.subscribe("rootTopic/topic/uplink")
+    client.loop_start()
+
+    client.publish(topic=topic, payload=payload)
+    # time.sleep(5)
+
+    client.loop_stop()
+    client.disconnect()
