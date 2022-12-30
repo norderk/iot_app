57be7a07e9ec84a53ef5ae27704b2ef9c197d5b2
norderk <33392181+norderk@users.noreply.github.com>
2022-12-30 15:33:08 +0100

    Revert "App in django"

--- django_iot/my_iot_site/home/static/java/imgButton.js
+++
@@ -1,7 +0,0 @@
-function getPage(page_name) {
-    if(page_name == '' | page_name == 'home'){
-        document.location.href="/";
-    } else {
-        location.replace(page_name)
-    }
-}
--- django_iot/my_iot_site/home/static/python/__init__.py
+++
@@ -1,4 +0,0 @@
-import  os
-os.environ(MQTT_BROKER_HOST="192.168.0.17")
-os.environ(MQTT_BROKER_HOST=1883)
-os.environ(MQTT_KEEP_ALIVE_INTERVAL=60)
