24729aca9645fd11ae68620b18d8db91b4dea171
erik nordqvist <nordqvist.eriken@gmail.com>
2024-01-21 11:16:10 +0100

    add js to django

--- django_iot/my_iot_site/home/static/java/netApp.js
+++ django_iot/my_iot_site/home/static/java/netApp.js
@@ -2,18 +2,9 @@ const menu = document.querySelector('.menu')
 const close = document.querySelector('.close')
 const nav = document.querySelector('nav')
 
-// function goPython(){
-//         $.ajax({
-//         url: "MYSCRIPT.py",
-//         context: document.body
-//         }).done(function() {
-//         alert('finished python script');;
-//         });
-//     }
-
 menu.addEventListener('click', () => {
-    nav.classList.add('open-nav')
+  nav.classList.add('open-nav')
 })
 close.addEventListener('click', () => {
-    nav.classList.remove('open-nav')
+  nav.classList.remove('open-nav')
 })
