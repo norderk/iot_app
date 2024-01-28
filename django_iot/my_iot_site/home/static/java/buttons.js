678461623717509b3502e8262d613a6b29b72e35
erik nordqvist <nordqvist.eriken@gmail.com>
2024-01-28 17:55:26 +0100

    adds extended logger for api

--- django_iot/my_iot_site/home/static/java/buttons.js
+++ django_iot/my_iot_site/home/static/java/buttons.js
@@ -3,7 +3,7 @@
 function getOnOff (zone, payload) {
   /*`http://fast_api:80/zigbee/?topic=zigbee2mqtt/${zone}/set&payload=${payload}` <-- Inside docker network*/
   /*`http://localhost:2222/zigbee/?topic=zigbee2mqtt/${zone}/set&payload=${payload}` <-- On localhost*/
-  fetch(`http://fast_api/zigbee/?topic=zigbee2mqtt/${zone}/set&payload=${payload}`)
+  fetch(`http://localhost:8001/zigbee/?topic=zigbee2mqtt/${zone}/set&payload=${payload}`)
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok')
